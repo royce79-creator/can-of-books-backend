e86265126a142e1283fa0e6579ee8ecd1415c668
royce79-creator <connorsboyce07@gmail.com>
2021-08-18 22:44:48 -0700

    Made changes for lab 12, still need to make changes

---
+++ books.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  email: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, uppercase: true, enum: ['LIFE-CHANGING', 'FAVORITE FIVE', 'RECCOMENDED TO ME'] },
+});
+
+const BookModel = mongoose.model('books', bookSchema);
+
+module.exports = BookModel;
