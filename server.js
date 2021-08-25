2136b073ae5ff4e4c2319c18fe3545fe4f912598
royce79-creator <connorsboyce07@gmail.com>
2021-08-24 21:08:55 -0700

    Made changes to finish up my lab-12

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 const mongoose = require('mongoose');
-const BookModel = require('./books')
+const BookModel = require('./books');
+const { response } = require('express');
 
 const app = express();
 app.use(cors());
@@ -31,35 +32,105 @@ app.get('/', (request, response) => {
 
 // TODO: 
 // STEP 1: get the jwt from the headers
-app.get('/test-login', (request, response) => {
-  // grab the token sent by the front end
-  const token = request.headers.authorization.split(' ')[1];
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-  jwt.verify(token, getKey, {}, function (err, user) {
-    if (err) {
-      response.status(500).send('invlaid token');
-    } else {
-      BookModel.find((err, ))
-    }
-    response.send(user);
-  });
-});
-
 app.get('/books', (request, response) => {
+  try {
+    // grab the token sent by the front end
+    const token = request.headers.authorization.split(' ')[1];
+    // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+    // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
 
+    // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+    jwt.verify(token, getKey, {}, function (err, user) {
+      if (err) {
+        response.status(500).send('invlaid token');
+      } else {
+        BookModel.find({}, (err, dataBaseResults) => {
+          if (err) {
+            response.send('can\'t access DB');
+          } else {
+            response.status(200).send(dataBaseResults);
+          }
+        });
+      }
+    });
+  }
+  catch (err) {
+    response.status(500).send('database error');
+  }
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// app.get('/books', async (request, response) => {
+//   try {
 
-app.listen(3001, () => {
-  console.log('Server up on 3001');
+//     let booksDataBase = await BookModel.find({});
+//     response.status(200).send(booksDataBase);
+//   }
+//   catch (err) {
+//     response.status(500).send('Database error');
+//   }
+// })
 
-  async function addBook(obj) {
-    //CREATE R U D
-    //{title: "", author: ""}
-    let newBook = new BookModel(obj);
-    return await newBook.save();
-  }
+//CODE THAT WAS USED WITHIN CLASS WITH TA'S
+// app.get('/books', (request, response) => {
+//   try {
+//     const token = request.headers.authorization.split(' ')[1];
+//     jwt.verify(token, getKey, {}, function (err, user) {
+//       if (err) {
+//         response.status(500).send('invalid token');
+//       } else {
+//         let emailAuth = token.email;
+//         BookModel.find({ emailAuth }, (err, dataBaseResults) => {
+//           if (err) {
+//             response.send('can\'t access DB');
+//           } else {
+//             response.status(200).send(dataBaseResults);
+//           }
+//         })
+//       }
+//     })
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+// });
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/books', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+  .then(async () => {
+    console.log('connected to database');
+
+    app.listen(3001, () => {
+      console.log('Server up on 3001');
+    });
+  });
+
+async function addBook(obj) {
+  //CREATE R U D
+  //{title: "", author: ""}
+  let newBook = new BookModel(obj);
+  return await newBook.save();
+}
+app.get('/seed', seed);
+
+function seed(request, response) {
+  console.log('seeding');
+  addBook({
+    title: 'The Odyssey',
+    description: 'Odyssues and his mom have issues',
+    email: 'delightingreen@gmail.com',
+  })
+  addBook({
+    title: 'The Giver',
+    description: 'Loss of color',
+    email: 'delightingreen@gmail.com',
+  })
+  addBook({
+    title: 'The Odyssey',
+    description: 'Odyssues and his mom have issues',
+    email: 'delightingreen@gmail.com',
+  })
+  response.send('data base seeded');
+};
